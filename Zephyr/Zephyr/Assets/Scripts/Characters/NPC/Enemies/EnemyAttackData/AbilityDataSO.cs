5fcd22b583f70e95eae847c36ab55b3e08af5133
fhgggtyf <xjc18502795710@outlook.com>
2024-08-23 11:57:50 +0800

    backup before gravity change

--- Zephyr/Zephyr/Assets/Scripts/Characters/NPC/Enemies/EnemyAttackData/AbilityDataSO.cs
+++ Zephyr/Zephyr/Assets/Scripts/Characters/NPC/Enemies/EnemyAttackData/AbilityDataSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "AbilityData", menuName = "AbilityData")]
+[CreateAssetMenu(fileName = "AbilityData", menuName = "AbilityDat")]
 public class AbilityDataSO : ScriptableObject
 {
     public DamageType type;
